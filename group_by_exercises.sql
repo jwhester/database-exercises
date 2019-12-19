24ada1b6a70747babde45d6ebfb0c3de20e0b017
jwhester <jameshester88@gmail.com>
2019-12-19 11:57:54 -0600

    finished

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -7,18 +7,18 @@ FROM titles;
 SELECT last_name
 FROM employees
 WHERE last_name like 'E%E'
-GROUP BY last_name
+GROUP BY last_name;
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name like 'E%E'
-GROUP BY first_name, last_name
+GROUP BY first_name, last_name;
 
 
 SELECT last_name
 FROM employees
 WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%'
-GROUP BY last_name
+GROUP BY last_name;
 
 SELECT first_name, last_name
 from employees
